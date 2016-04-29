2a08c02aefa83335b9fb26a2665562c424a971d5
Seth <s.weinberg62@gmail.com>
2016-04-29 18:21:20 -0400

    Initial Commit

---
+++ Assets/Scripts/Shared/InfoEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class InfoEventArgs<T> : EventArgs
+{
+    public T info;
+
+    public InfoEventArgs()
+    {
+        info = default(T);
+    }
+    public InfoEventArgs(T info)
+    {
+        this.info = info;
+    }
+}
